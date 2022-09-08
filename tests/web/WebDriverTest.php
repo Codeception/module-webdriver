f53684626680df3fd31154f6235da4962a704b11
Meindert-Jan Kroese <mkroese@eljakim.nl>
2022-09-08 10:41:00 +0200

    prevent closing the final tab, as it will destroy the session

--- tests/web/WebDriverTest.php
+++ tests/web/WebDriverTest.php
@@ -1039,7 +1039,6 @@ class WebDriverTest extends TestsForBrowsers
         $this->module->closeTab();
         $this->module->seeNumberOfTabs(2);
         $this->module->closeTab();
-        $this->module->closeTab();
     }
 
     public function testPerformOnWithArray()
