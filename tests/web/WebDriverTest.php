1b0fac8bac04f17c6c15c7f3bc0ab13da341f06b
Gintautas Miselis <gintautas@miselis.lt>
2023-02-03 23:40:03 +0200

    Move module unit tests to unit suite

--- tests/web/WebDriverTest.php
+++ tests/web/WebDriverTest.php
@@ -7,8 +7,6 @@ namespace Tests\Web;
 use Codeception\Module\WebDriver;
 use Codeception\Stub;
 use Codeception\Stub\Expected;
-use Codeception\Test\Metadata;
-use Codeception\Util\Maybe;
 use data;
 use Facebook\WebDriver\Cookie;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
@@ -567,82 +565,6 @@ final class WebDriverTest extends TestsForBrowsers
         $module = Stub::make(self::MODULE_CLASS, ['webDriver' => $fakeWd]);
             $cept = (new \Codeception\Test\Cept('loginCept', 'loginCept.php'));
         $module->_failed($cept, new \PHPUnit\Framework\AssertionFailedError());
-    }
-
-    public function testCreateCestScreenshotOnFail()
-    {
-        $fakeWd = Stub::make(self::WEBDRIVER_CLASS, [
-            'takeScreenshot' => Expected::once(function ($filename) {
-                Assert::assertSame(codecept_log_dir('stdClass.login.fail.png'), $filename);
-            }),
-            'getPageSource' => Expected::once(function () {
-            }),
-            'manage' => Stub::make('\Facebook\WebDriver\WebDriverOptions', [
-                'getAvailableLogTypes' => Expected::atLeastOnce(fn() => []),
-            ]),
-        ]);
-        $module = Stub::make(self::MODULE_CLASS, ['webDriver' => $fakeWd]);
-        $cest = new \Codeception\Test\Cest(
-            new class {
-                public function login()
-                {
-                }
-            },
-            'login',
-            'someCest.php',
-        );
-        $module->_failed($cest, new \PHPUnit\Framework\AssertionFailedError());
-    }
-
-    public function testCreateTestScreenshotOnFail()
-    {
-        $test = Stub::make(
-            \Codeception\Test\TestCaseWrapper::class,
-            [
-                'getSignature' => 'testLogin',
-                'getMetadata' => new Metadata(),
-            ]
-        );
-        $fakeWd = Stub::make(self::WEBDRIVER_CLASS, [
-            'takeScreenshot' => Expected::once(function ($filename) use ($test) {
-                Assert::assertSame(
-                    codecept_log_dir(get_class($test) . '.testLogin.fail.png'),
-                    $filename
-                );
-            }),
-            'getPageSource' => Expected::once(function () {
-            }),
-            'manage' => Stub::make('\Facebook\WebDriver\WebDriverOptions', [
-                'getAvailableLogTypes' => Expected::atLeastOnce(fn() => []),
-            ]),
-        ]);
-        $module = Stub::make(self::MODULE_CLASS, ['webDriver' => $fakeWd]);
-        $module->_failed($test, new \PHPUnit\Framework\AssertionFailedError());
-    }
-
-    public function testWebDriverWaits()
-    {
-        $fakeWd = Stub::make(self::WEBDRIVER_CLASS, ['wait' => Expected::exactly(16, fn() => new Maybe())]);
-        $module = Stub::make(self::MODULE_CLASS, ['webDriver' => $fakeWd]);
-        $module->waitForElement(WebDriverBy::partialLinkText('yeah'));
-        $module->waitForElement(['id' => 'user']);
-        $module->waitForElement(['css' => '.user']);
-        $module->waitForElement('//xpath');
-
-        $module->waitForElementVisible(WebDriverBy::partialLinkText('yeah'));
-        $module->waitForElementVisible(['id' => 'user']);
-        $module->waitForElementVisible(['css' => '.user']);
-        $module->waitForElementVisible('//xpath');
-
-        $module->waitForElementNotVisible(WebDriverBy::partialLinkText('yeah'));
-        $module->waitForElementNotVisible(['id' => 'user']);
-        $module->waitForElementNotVisible(['css' => '.user']);
-        $module->waitForElementNotVisible('//xpath');
-
-        $module->waitForElementClickable(WebDriverBy::partialLinkText('yeah'));
-        $module->waitForElementClickable(['id' => 'user']);
-        $module->waitForElementClickable(['css' => '.user']);
-        $module->waitForElementClickable('//xpath');
     }
 
     public function testWaitForElement()
